f33a18426977eabdffe00f1a6036ee5df0675e3f
Murali Kolli <163140529+Murali-295@users.noreply.github.com>
2024-05-10 13:15:39 +0530

    Add files via upload

---
+++ Spring-Json-T1/src/main/java/com/mk/SpringJsonT1Application.java
@@ -0,0 +1,13 @@
+package com.mk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJsonT1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJsonT1Application.class, args);
+	}
+
+}
